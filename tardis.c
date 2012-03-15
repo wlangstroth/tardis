c5d7b72bede330de38576e2285bbcf9adaf0efb3
Will Langstroth <will@langstroth.com>
2012-03-15 09:40:39 -0400

    and done

--- tardis.c
+++ tardis.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int
 main (int args, char *argv[])
 {
-  return EXIT_SUCCESS;
+  FILE *fp;
+  time_t now;
+
+  time(&now);
+
+  fp = fopen("log.txt","a");
+
+  fprintf(fp, "%s - %s", argv[1], ctime(&now));
+
+  fclose(fp);
+
+  exit(EXIT_SUCCESS);
 }
