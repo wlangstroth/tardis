6e6caa05773459fd2c83bc820242789f80038c86
Will Langstroth <will@langstroth.com>
2012-04-16 07:13:31 -0400

    finish basic timestamping

--- tardis.c
+++ tardis.c
@@ -6,9 +6,10 @@
 #define BUFFER_LENGTH 4096
 #define DATE_LENGTH    128
 
+// Forward declarations for sqlite3_exec() callbacks
+static int post_create(void *, int, char **, char **);
 static int post_update(void *, int, char **, char **);
 static int post_insert(void *, int, char **, char **);
-static int post_select(void *, int, char **, char **);
 
 int
 main(int argc, char *argv[])
@@ -19,9 +20,6 @@ main(int argc, char *argv[])
   char          update_sql[BUFFER_LENGTH];
   char          insert_sql[BUFFER_LENGTH];
   char          date_buffer[DATE_LENGTH];
-  static char  *update_template = "update entries set end='%s' where id = (select max(id) from entries);";
-  static char  *insert_template = "insert into entries(start, project, description) values('%s','%s');";
-  static char  *select_sql = "select * from entries";
   time_t        rawtime;
   struct tm    *timeinfo;
 
@@ -33,14 +31,9 @@ main(int argc, char *argv[])
   char *project = argv[1];
   char *description = argv[2];
 
-  time(&rawtime);
-  timeinfo = localtime(&rawtime);
-  strftime(date_buffer, DATE_LENGTH, "%Y-%m-%d %H:%M:%S", timeinfo);
-  sprintf(update_sql, update_template, date_buffer);
-  sprintf(insert_sql, insert_template, project, description);
 
   // This will create a new log.db file if one does not exist, but will not
-  // populate it with an entries table
+  // populate it with an entries table, so we have to do that.
   result_code = sqlite3_open("log.db", &db);
   if (result_code == SQLITE_CANTOPEN) {
     fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
@@ -48,19 +41,29 @@ main(int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
 
+  static char  *create_sql = "create table if not exists entries(id integer primary key autoincrement, start datetime default current_timestamp, project text, description text, end datetime)";
+  result_code = sqlite3_exec(db, create_sql, post_create, 0, &error_message);
+  if (result_code) {
+    fprintf(stderr, "SQL error: %s\n", error_message);
+    sqlite3_free(error_message);
+  }
+
+  static char  *update_template = "update entries set end='%s' where id = (select max(id) from entries);";
+  static char  *time_format = "%Y-%m-%d %H:%M:%S";
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  strftime(date_buffer, DATE_LENGTH, time_format, timeinfo);
+
+  sprintf(update_sql, update_template, date_buffer);
   result_code = sqlite3_exec(db, update_sql, post_update, 0, &error_message);
   if (result_code) {
     fprintf(stderr, "SQL error: %s\n", error_message);
     sqlite3_free(error_message);
   }
 
+  static char  *insert_template = "insert into entries(project, description) values('%s','%s');";
+  sprintf(insert_sql, insert_template, project, description);
   result_code = sqlite3_exec(db, insert_sql, post_insert, 0, &error_message);
-  if (result_code) {
-    fprintf(stderr, "SQL error: %s\n", error_message);
-    sqlite3_free(error_message);
-  }
-
-  result_code = sqlite3_exec(db, select_sql, post_select, 0, &error_message);
   if (result_code) {
     fprintf(stderr, "SQL error: %s\n", error_message);
     sqlite3_free(error_message);
@@ -71,14 +74,8 @@ main(int argc, char *argv[])
 }
 
 static int
-post_select(void *not_used, int argc, char *argv[], char *az_col_name[]) {
-  int i;
-
-  for (i = 0; i < argc; i++) {
-    printf("%s = %s\n", az_col_name[i], argv[i] ? argv[i] : "NULL");
-  }
-
-  printf("\n");
+post_create(void *not_used, int argc, char *argv[], char *az_col_name[]) {
+  printf("table created\n");
   return 0;
 }
 
