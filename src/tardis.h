ad90c59f43043edc713d10f83324a3f9b60edac9
Will Langstroth <will@langstroth.com>
2013-09-09 08:02:52 -0400

    Format available commands string

--- src/tardis.h
+++ src/tardis.h
@@ -11,7 +11,16 @@
 #define BUFFER_LENGTH 4096
 #define TIME_LENGTH   64
 #define DATE_LENGTH   20
-#define AVAILABLE     "Available commands:\nadd\nb[ackup]\nend\nlast\nlist (ls)\nr[eport]\ns[tart]\nstop\nt[ask]\n"
+#define AVAILABLE_COMMANDS "Available commands:\n\
+	add\n\
+	b[ackup]\n\
+	end\n\
+	last\n\
+	list (ls)\n\
+	r[eport]\n\
+	s[tart]\n\
+	stop\n\
+	t[ask]\n"
 
 int time_string_to_seconds(char *);
 char *seconds_to_time_string(int);
